bcdaf7920885cba7d70dc2a95df2d661ff9de424
Priscilla Del Toro <priscilladeltoro10@gmail.com>
2019-12-23 12:36:41 -0800

    added basic carousel

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
@@ -8,7 +8,30 @@ get_header(); ?>
 	<main role="main">
 		<!-- section -->
 		<section>
+			<div id="demo" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+				<ul class="carousel-indicators">
+  					<li data-target="#demo" data-slide-to="0" class="active"></li>
+  					<li data-target="#demo" data-slide-to="1"></li>
+  					<li data-target="#demo" data-slide-to="2"></li>
+					<li data-target="#demo" data-slide-to="3"></li>
+				</ul>
 
+		<!-- The slideshow -->
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Green-Holiday-with-Yellow.jpg" alt="Green holiday">
+  				</div>
+				<div class="carousel-item">
+					<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Backpacks-scaled.jpg" alt="Backpacks">
+  				</div>
+  				<div class="carousel-item">
+					<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Holiday-Packaging.jpg" alt="Holiday packaging">
+  				</div>
+				<div class="carousel-item">
+					<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Women-to-Right-scaled.jpg" alt="Women to right">
+  				</div>
+			</div>
 		</section>
 		<!-- /section -->
 	</main>
