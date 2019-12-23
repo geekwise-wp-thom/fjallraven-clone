2b1864c106f1830d720f89b48eb1db640984c2a9
Priscilla Del Toro <priscilladeltoro10@gmail.com>
2019-12-23 15:55:30 -0800

    added buttons and captions to carousel

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
@@ -6,34 +6,51 @@
 get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
 		<section>
-			<div id="demo" class="carousel slide" data-ride="carousel">
-		<!-- Indicators -->
-				<ul class="carousel-indicators">
-  					<li data-target="#demo" data-slide-to="0" class="active"></li>
-  					<li data-target="#demo" data-slide-to="1"></li>
-  					<li data-target="#demo" data-slide-to="2"></li>
-					<li data-target="#demo" data-slide-to="3"></li>
-				</ul>
-
-		<!-- The slideshow -->
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Green-Holiday-with-Yellow.jpg" alt="Green holiday">
-  				</div>
-				<div class="carousel-item">
-					<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Backpacks-scaled.jpg" alt="Backpacks">
-  				</div>
-  				<div class="carousel-item">
-					<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Holiday-Packaging.jpg" alt="Holiday packaging">
-  				</div>
-				<div class="carousel-item">
-					<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Women-to-Right-scaled.jpg" alt="Women to right">
-  				</div>
+			<div class="carousel slide fluid-container" data-ride="carousel">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Green-Holiday-with-Yellow.jpg" alt="Green holiday">
+						<div class="carousel-caption">
+        					<p class="button" style="float: center;"><strong>God Jul!</strong></p>
+      					</div>  
+  					</div>
+					<div class="carousel-item">
+						<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Backpacks-scaled.jpg" alt="Backpacks">
+						<div class="carousel-caption">
+       						<h1 style="font-size: 32px;">FUN FOR THE HOLIDAYS</h1>
+        					<p class="button" style="float: center;">SHOP <strong>KANKEN</strong></p>
+      					</div> 
+  					</div>
+  					<div class="carousel-item">
+						<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Holiday-Packaging.jpg" alt="Holiday packaging">
+						<div class="carousel-caption">
+       						<h1 style="font-size: 32px;">GIVE THE GIFT OF FJALLRAVEN</h1>
+        					<p class="button" style="float: center;">SHOP <strong>GIFT GUIDE</strong></p>
+      					</div> 
+  					</div>
+					<div class="carousel-item">
+						<img src="http://fjallraven.clone/wp-content/uploads/2019/12/Header-Carousel-Women-to-Right-scaled.jpg" alt="Women to right">
+						<div class="carousel-caption" style="text-align: left;">
+       						<h1 style="font-size: 32px; color: gray;">STAY WARM THIS WINTER</h1>
+        					<p class="button" style="float: center;">SHOP <strong>PARKAS</strong></p>
+      					</div> 
+  					</div>
+				</div>
+			</div>
+			<div class="row flex-container">
+				<div class="col"></div>
+  				<div class="col">
+    				<div class="row">
+						  <div class="col">God Jul</div>
+						  <div class="col">Kanken</div>
+						  <div class="col">Gift Guide</div>
+						  <div class="col">Parkas</div>
+					</div>
+				</div>
+				<div class="col"></div>
 			</div>
 		</section>
-		<!-- /section -->
 	</main>
 
 
