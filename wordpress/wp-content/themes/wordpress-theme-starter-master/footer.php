347e9e662f3a301a7cc02f48c093ac5a0789c464
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2019-12-31 11:21:04 -0800

    saving added footer work

--- wordpress/wp-content/themes/wordpress-theme-starter-master/footer.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/footer.php
@@ -1,5 +1,53 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
+				<div class="row container">
+					<div class="col">
+						<div class="group">
+							<label class="title">Customer Service</label>
+							<ul class="links">
+								<li clas="link">Contact Us</li>
+								<li clas="link">FAQ</li>
+								<li clas="link">Shipping</li>
+								<li clas="link">Returns</li>
+								<li clas="link">Payments</li>
+								<li clas="link">Buyers Guide</li>
+								<li clas="link">Care & Repair</li>
+								<li clas="link">Size Guide</li>
+						</div>
+					</div>
+					<div class="col">
+						<div class="group">
+							<label class="title">About Fjällräven</label>
+							<ul class="links">
+								<li clas="link">About Us</li>
+								<li clas="link">Sustainability</li>
+								<li clas="link">Our Materials</li>
+								<li clas="link">Careers</li>
+								<li clas="link">Press Room</li>
+						</div>
+						<div class="group">
+							<label class="title">Legal</label>
+							<ul class="links">
+								<li clas="link">Terms & Conditions</li>
+								<li clas="link">Legal Notice</li>
+								<li clas="link">Privacy & Data Protection</li>
+								<li clas="link">Accessibility Statement</li>
+						</div>
+					</div>
+					<div class="col">
+					<div class="group">
+							<label class="title">Our Adventures</label>
+							<ul class="links">
+								<li clas="link">Fjällräven Polar</li>
+								<li clas="link">Fjällräven Classic</li>
+								<li clas="link" style="text-decoration: underline;">
+                                        Opt-In for Google Tracking
+                                    </li>
+					</div>
+				</div>
+
+			</footer>
+			<!-- /footer -->
 
 				<!-- copyright -->
 				<p class="copyright">
@@ -7,10 +55,6 @@
 					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
 				</p>
 				<!-- /copyright -->
-
-			</footer>
-			<!-- /footer -->
-
 		</div>
 		<!-- /wrapper -->
 
