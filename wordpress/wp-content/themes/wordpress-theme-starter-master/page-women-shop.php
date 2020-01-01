92a8cccc787486c74128bff4fd316ab0ca1ef710
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-01-01 12:16:52 -0800

    saving added products

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-women-shop.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-women-shop.php
@@ -13,11 +13,7 @@ get_header(); ?>
 						<h1>Women</h1>
 					</div>
 				</div>
-			</div>
-		</section>
-		<section>
-			<div class="shop container">
-				<div class="browse">
+				<div class="shop">
 					<div class="sidebar">
 						<div class="link">Material Name</div>
 						<div class="link">Color</div>
@@ -25,14 +21,70 @@ get_header(); ?>
 						<div class="link">Family</div>
 						<div class="link">Environmental Info</div>
 						<div class="link">Women</div>
-						<div class="link">
+						<div class="link" style="border-bottom: none;">
 							<ul class="sub-link">
-								<li>Jackets</li>
-								<li>Tops</li>
-								<li>Bottoms</li>
-								<li>Accessories</li>
-								<li>Season</li>
+								<li class="link">Jackets</li>
+								<li class="link">Tops</li>
+								<li class="link">Bottoms</li>
+								<li class="link">Accessories</li>
+								<li class="link">Season</li>
 							</ul>
+						</div>
+					</div>
+					<div class="product">
+						<div class="item">
+							<div class="item-image">
+								<img src="http://fjallraven.clone/wp-content/uploads/2020/01/1a.jpg" alt="item1">
+							</div>
+								<div class="item-colors">
+
+								</div>
+							<div class="item-info">
+								<h1>Marlin Shade Hat</h1>
+								<div class="item-price">$45.00</div>
+							</div>
+						</div>
+					</div>
+					<div class="product">
+						<div class="item">
+							<div class="item-image">
+								<img src="http://fjallraven.clone/wp-content/uploads/2020/01/2.jpg" alt="item2">
+							</div>
+							<div class="item-colors">
+
+								</div>
+							<div class="item-info">
+								<h1>Nordic Heater</h1>
+								<div class="item-price">$80.00</div>
+							</div>
+						</div>
+					</div>
+					<div class="product">
+						<div class="item">
+							<div class="item-image">
+								<img src="http://fjallraven.clone/wp-content/uploads/2020/01/3.jpg" alt="item3">
+							</div>
+							<div class="item-colors">
+
+								</div>
+							<div class="item-info">
+								<h1>Abisko Pack Cap</h1>
+								<div class="item-price">$35.00</div>
+							</div>
+						</div>
+					</div>
+					<div class="product">
+						<div class="item">
+							<div class="item-image">
+								<img src="http://fjallraven.clone/wp-content/uploads/2020/01/4.jpg" alt="item4">
+							</div>
+								<div class="item-colors">
+
+								</div>
+							<div class="item-info">
+								<h1>Singi Balaclava Cap</h1>
+								<div class="item-price">$50.00</div>
+							</div>
 						</div>
 					</div>
 				</div>
