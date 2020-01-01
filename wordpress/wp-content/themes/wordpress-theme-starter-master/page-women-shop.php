12ad9d0f37d9af1915d451a6107405f05f44b3f1
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-01-01 11:09:33 -0800

    adding side menu

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-women-shop.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-women-shop.php
@@ -17,7 +17,25 @@ get_header(); ?>
 		</section>
 		<section>
 			<div class="shop container">
-				
+				<div class="browse">
+					<div class="sidebar">
+						<div class="link">Material Name</div>
+						<div class="link">Color</div>
+						<div class="link">Activity</div>
+						<div class="link">Family</div>
+						<div class="link">Environmental Info</div>
+						<div class="link">Women</div>
+						<div class="link">
+							<ul class="sub-link">
+								<li>Jackets</li>
+								<li>Tops</li>
+								<li>Bottoms</li>
+								<li>Accessories</li>
+								<li>Season</li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	</main>
